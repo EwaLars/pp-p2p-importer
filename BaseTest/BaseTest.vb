d9ff4391dcb3a7867dcd7689c0f1896c28d65430
Lars Kempa <kempa@it-easy.de>
2019-05-12 19:47:30 +0200

    Added first unit tests

---
+++ BaseTest/BaseTest.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Base
+
+<TestClass()> Public Class BaseTest
+
+    <TestMethod()>
+    Public Sub Fkt_UpperCaseFirstLetter_Working()
+
+
+
+        Dim expected As String = "Welt"
+        Dim input As String = "welt"
+
+        Dim result = Fkt.UppercaseFirstLetter(input)
+
+        Assert.AreEqual(expected, result)
+    End Sub
+
+End Class
---
+++ BaseTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("BaseTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("BaseTest")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("5825846e-1067-4775-b421-7f27cb09e14f")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
