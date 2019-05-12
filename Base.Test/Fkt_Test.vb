3ecbff0abff462770c00be54e4c6e3aa88472bc5
Lars Kempa <kempa@it-easy.de>
2019-05-12 20:37:09 +0200

    Changed unit testing from MS to Xunit

---
+++ Base.Test/Fkt_Test.vb
@@ -0,0 +1,17 @@
+﻿Imports Xunit
+
+Public Class Fkt_Test
+
+
+
+    <Fact>
+    Public Sub Fkt_UpperCaseFirstLetter()
+        Dim expected As String = "Welt"
+        Dim input As String = "welt"
+
+        Dim result = Fkt.UppercaseFirstLetter(input)
+
+        Assert.Equal(expected, result)
+    End Sub
+
+End Class
