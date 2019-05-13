099c5a1e12f013b8b1df05a0ee2f3fe77548921b
Lars Kempa <kempa@it-easy.de>
2019-05-13 10:05:36 +0200

    Integration of Unit Tests and AppVeyor (#1)
    * Removed unused dependencies
    
    * Added first unit tests
    
    * NUnit Adapter added
    
    * Updated NuGet packages and downgraded BaseTest to .NET v4.6
    
    * Create README.md
    
    * Changed unit testing from MS to Xunit
    
    * Removed some more NuGet Packages
    
    * Added some more unit tests
    
    * Added a failing unit test
    
    * Removed failing unit test
    
    * Test AppVeyor Deployment
    
    * Added one more unit test
    
    * Removed useless unit test
    
    * Removed useless line breaks
    
    * One more line break deleted

---
+++ Base.Test/Fkt_Test.vb
@@ -0,0 +1,17 @@
+﻿Imports Xunit
+
+Public Class Fkt_Test
+
+
+
+    <Theory>
+    <InlineData("welt", "Welt")>
+    <InlineData("WELT", "WELT")>
+    <InlineData("", "")>
+    <InlineData("a", "A")>
+    <InlineData(Nothing, Nothing)>
+    Public Sub Fkt_UpperCaseFirstLetter(Input As String, Expected As String)
+        Dim result = Fkt.UppercaseFirstLetter(Input)
+        Assert.Equal(Expected, result)
+    End Sub
+End Class
